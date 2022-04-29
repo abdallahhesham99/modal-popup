ce0bb82082449c9a2cd720c3be37c50cd57f9342
abdallahhesham99 <abdallahhesham414@gmail.com>
2022-04-29 09:38:44 +0200

    method 2

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ let showModalButtons = document.querySelectorAll('.show-modal');
 let modals = document.querySelectorAll('.modal');
 let closeModalButtons = document.querySelectorAll('.close-modal');
 
-//===================== Method 1===========================
+//===================== Method 1====================================
+
 //show buttons
 showModalButtons.forEach((showbtn, modalIndex) =>
   showbtn.addEventListener('click', () => openModal(modalIndex))
@@ -27,9 +28,33 @@ function closeModal(index) {
 }
 
 //close modal through **overlay**
-
 window.addEventListener('click', e => {
   if (e.target.className === 'modal') {
     e.target.classList.add('hidden');
   }
 });
+
+//===================== Method 2===========================
+//show button
+
+// showModalButtons.forEach(btn => {
+//   btn.addEventListener('click', () => {
+//     let modal = btn.getAttribute('data-model');
+//     document.getElementById(modal).classList.remove('hidden');
+//   });
+// });
+//close button
+
+// closeModalButtons.forEach(btn => {
+//   btn.addEventListener('click', () => {
+//     btn.closest('.modal').classList.add('hidden');
+//   });
+// });
+
+//close modal through **overlay**
+
+// window.addEventListener('click', e => {
+//   if (e.target.className === 'modal') {
+//     e.target.classList.add('hidden');
+//   }
+// });
